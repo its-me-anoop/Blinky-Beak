0fd29bb10cc6ac55f1af695c734af9c1c8602b25
Anoop Jose <anoop@flutterly.co.uk>
2024-06-04 13:53:26 +0100

    Initial Commit

---
+++ Flappy Bird/Flappy_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flappy_BirdApp.swift
+//  Flappy Bird
+//
+//  Created by Anoop Jose on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flappy_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
